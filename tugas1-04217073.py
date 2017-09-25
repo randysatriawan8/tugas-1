84bd5f6435c5fd56cbe239a8cab99c8045549c93
randysatriawan8 <32242040+randysatriawan8@users.noreply.github.com>
2017-09-24 21:08:07 -0700

    Add files via upload
    data diri

---
+++ tugas1-04217073.py
@@ -0,0 +1,9 @@
+nama = "Randy Satriawan"
+nim = "04217073"
+umur = 18
+print ("namaku:" + nama)
+print ("nimku:" + nim)
+print ( "umur" )
+print (umur)
+print ("umur 5 tahun kedepan")
+print (umur +5)
